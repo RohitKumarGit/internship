43dff88928ba8acf9767eaf5ea3e1b09f3bc07a4
ROHIT KUMAR <mailrohitkr6@gmail.com>
2021-05-29 08:39:25 +0530

    bugs

--- setup.sql
+++ setup.sql
@@ -1,4 +1,5 @@
-
+CREATE DATABASE internship;
+use internship;
 CREATE TABLE `weather_data` (`city_id` INT NOT NULL AUTO_INCREMENT,`city_name` VARCHAR(255),`description` VARCHAR(255),`min_temp` FLOAT,`max_temp` FLOAT,`wind_speed` FLOAT,`cloud_perc` FLOAT,`curr_temp` FLOAT,`city_code` INT , PRIMARY KEY (`city_id`));
 
 
@@ -9,4 +10,3 @@ CREATE TABLE `userCity` (`user_id` INT NOT NULL,`city_code` INT);
 
  ALTER TABLE usercity ADD FOREIGN KEY(user_id) REFERENCES users(user_id);
 
- ALTER TABLE usercity ADD FOREIGN KEY(city_code) REFERENCES weather_data(city_code);
